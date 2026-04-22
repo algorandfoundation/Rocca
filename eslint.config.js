60745797eacc6675e5b3901b137a6318cdb3f1f4
Blair McIlroy <blair.mcilroy@algorand.foundation>
2026-04-22 20:42:30 +0700

    feat: add oxlint linter alongside ESLint with eslint-plugin-oxlint integration

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,13 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const oxlint = require('eslint-plugin-oxlint');
 
 module.exports = defineConfig([
   expoConfig,
+  // Disable ESLint rules already covered by oxlint to avoid duplication.
+  // See: https://oxc.rs/docs/guide/usage/linter/eslint-plugin-oxlint
+  oxlint.configs['flat/all'],
   {
     ignores: ['dist/*'],
   },
